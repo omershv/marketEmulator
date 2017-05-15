9877da683e5f8ab8a459fbf5325b93f51c66f926
Omer S <you@example.com>
2017-05-15 11:51:25 +0300

    Added SQL support

--- MarketTrader.py
+++ MarketTrader.py
@@ -1,7 +1,26 @@
 import MarketState
 import time
+import pyodbc
+import os
+import traceback
+import datetime
+
+sqlUsername = None
+if os.path.exists("credentials.txt"):
+	sqlUsername = open("credentials.txt").read().splitlines()[0]
+	sqlPass = open("credentials.txt").read().splitlines()[1]
+else:
+	print "No SQL credentails"
 
 def performTrades():
+	cnxn = None
+	if sqlUsername is not None:
+		try:
+			cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=history;UID=%s;PWD=%s'%(sqlUsername, sqlPass))
+			cnxn.timeout = 1
+			cursor = cnxn.cursor()
+		except:
+			print traceback.print_exc()
 	for commodity in MarketState.commodities:
 		buys = filter(lambda query:query.commodity == commodity and query.type == "buy", MarketState.activeQueries)
 		sells = filter(lambda query:query.commodity == commodity and query.type == "sell", MarketState.activeQueries)
@@ -24,3 +43,12 @@ def performTrades():
 						del MarketState.activeQueries[idx]
 					MarketState.marketHistory[commodity].append((time.time(), price, amount))
 					MarketState.marketHistory[commodity] = MarketState.marketHistory[commodity][-2000:]
+					if cnxn is not None:
+						try:
+							ts = datetime.datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+							seller = int(sell.user.split("user")[1])
+							buyer = int(buy.user.split("user")[1])
+							q = 'INSERT INTO "history"."dbo"."items" ("timestamp", "commodity", "amount", "price", "seller", "buyer") VALUES (\'%s\', \'%d\', \'%d\', \'%f\', \'%d\', \'%d\'); commit;'%(ts, commodity, amount, price, seller, buyer)
+							cursor.execute(q)
+						except:
+							print traceback.print_exc()
